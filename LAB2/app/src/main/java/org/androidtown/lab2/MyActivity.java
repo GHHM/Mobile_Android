3dd810abc84ab1021764f061e156315832b3dd6e
GHHM <zozon1330@naver.com>
2017-04-03 16:40:38 +0900

    add comments

--- LAB2/app/src/main/java/org/androidtown/lab2/MyActivity.java
+++ LAB2/app/src/main/java/org/androidtown/lab2/MyActivity.java
@@ -11,6 +11,14 @@ import android.widget.Toast;
 
 import static android.R.attr.value;
 
+
+/*My Activity
+* Input: Data(bundle type)
+* Output: none
+* Take button from xml resource file and make intent.
+* This intent has a role of sending information to
+* another activity(newActivity.java) when user click a button '추가'
+* */
 public class MyActivity extends AppCompatActivity {
 
     @Override
@@ -18,30 +26,36 @@ public class MyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        Button button = (Button) findViewById(R.id.button);
-        final EditText e1 = (EditText) findViewById(R.id.e1);
+        Button button = (Button) findViewById(R.id.button); //take button from xml
+        final EditText e1 = (EditText) findViewById(R.id.e1);   //take editText form xml
         final EditText e2 = (EditText) findViewById(R.id.e2);
 
+        /*When user click the button, run this function*/
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), newActivity.class);
-                intent.putExtra("name", e1.getText().toString());
-                intent.putExtra("age",e2.getText().toString());
-                startActivityForResult(intent, 1);
+                Intent intent = new Intent(getApplicationContext(), newActivity.class); //Make new intent that has a message for newActivity.class
+                intent.putExtra("name", e1.getText().toString());   //Put an information which has value name "name" to intent
+                intent.putExtra("age",e2.getText().toString()); //Put an information which has value name as "age" to intent
+                startActivityForResult(intent, 1);  //Send intent to new Activity about intent data and result
 
             }
         });
     }
+
+    /*OnActivityResult
+    * input: requestCode(integer type), resultCode(integer), data (intent type)
+    * When this activity gets result from another activity,
+    * check the result and show the result in toast.
+    * */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-      //  Toast.makeText(this,"aaaaa",Toast.LENGTH_LONG).show();
-        try{
+         try{
             if((requestCode == 1) && resultCode == Activity.RESULT_OK){
-                    Bundle bundle = data.getExtras();
+                    Bundle bundle = data.getExtras();   //Typecast data(intent type) to bundle (bundle type)
                 Toast.makeText(this,
-                        bundle.getString("value"),Toast.LENGTH_LONG).show();
+                        bundle.getString("value"),Toast.LENGTH_LONG).show();    //Show received data as toast
             }
         }catch(Exception e){}
     }
--- LAB2/app/src/main/java/org/androidtown/lab2/newActivity.java
+++ LAB2/app/src/main/java/org/androidtown/lab2/newActivity.java
@@ -8,32 +8,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class newActivity extends AppCompatActivity {
+import static android.R.attr.value;
 
+public class newActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
-        final Intent passedIntent = getIntent();
-        if(passedIntent!= null){
-        String loginName = passedIntent.getStringExtra("name");
-            String loginAge = passedIntent.getStringExtra("age");
-            Toast.makeText(getApplication(), "Student Info : " + loginName+", "+loginAge, Toast.LENGTH_LONG).show();
+        final Intent passedIntent = getIntent();    //Get intent from MyActivity.java
+        if(passedIntent!= null){    //If the intent has some data
+        String loginName = passedIntent.getStringExtra("name");     //Find data which has value name "name"
+            String loginAge = passedIntent.getStringExtra("age");  //Find data which has value name "age"
+            Toast.makeText(getApplication(), "Student Info : " + loginName+", "+loginAge, Toast.LENGTH_LONG).show();    //Show received data from intent
         }else
         {
-            Toast.makeText(this,"No data",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"No data",Toast.LENGTH_LONG).show();    //if there is no data, toast "No data"
         }
 
-        Button button2 = (Button) findViewById(R.id.button2);
-
+        Button button2 = (Button) findViewById(R.id.button2);   // Find button "닫기"
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                passedIntent.putExtra("value","OK~");
-                setResult(Activity.RESULT_OK,passedIntent);
-                finish();
+            public void onClick(View v) { //When this button clicked
+                passedIntent.putExtra("value","OK~");   //send data "OK~" which value name is "value" through intent
+                setResult(Activity.RESULT_OK,passedIntent); //set the result as intent 'passedIntent'
+                finish();   //close the activity
             }
         });
     }
